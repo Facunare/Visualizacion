57ce662461503ffdc1bacc0756ed4afa63e89ab5
facunare <arechaga.facundoet36@gmail.com>
2026-04-25 23:12:30 -0300

    nueva pagina

--- Paginavdd/VDD/src/Footer.jsx
+++ Paginavdd/VDD/src/Footer.jsx
@@ -4,8 +4,15 @@ import SectionNumber from "./SectionNumber"
 function SixthGraph() {
   return (
     <footer>
-        <p>Datos: FIFA — Copa del Mundo 1930–2022</p>
-        <p>Visualizaciones: Flourish</p>
+        <div>
+          <p>Parcial Visualización de Datos- Tecnología Digital</p>
+          <p>Facundo Arechaga, Franco Bergman y Peter Ahn</p>
+
+        </div>
+        <div>
+          <p>Datos: FIFA — Copa del Mundo 1930–2022</p>
+          <p>Visualizaciones: Flourish</p>
+        </div>
     </footer>
   )
 }
